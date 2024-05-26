ae1dc7a08ab07ea59fe55fb6b0f29efb9cada598
h4z2 <h4z2webb@gmail.com>
2024-05-26 18:52:20 +1000

    update

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -16,3 +16,32 @@ updateCountdown(); // Initial call
 
 // Update the countdown every second
 setInterval(updateCountdown, 1000);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('rsvp-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const form = event.target;
+        fetch(form.action, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.result === 'success') {
+                    window.location.href = data.redirect;
+                } else {
+                    alert('An error occurred: ' + data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('An error occurred. Please try again.');
+            });
+    });
+});
